f4e6d5f51b463154a66398e51de55751da82b692
G VIKRAM <97166893+Vk4011@users.noreply.github.com>
2023-03-06 08:10:57 +0530

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,49 @@
+
+
+let sections =document.querySelectorAll('section');
+let navLinks =document.querySelectorAll('header nav a');
+
+
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height =  sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+
+        if(top >=offset && top <offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header  nav a[href*=' +id + ']').classList.add('active');
+
+            });
+        };
+    });
+    let header =document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+};
+
+
+const typed = new Typed ('.multiple-text',{
+    strings:['Frontend Developer','Backend','Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backSpeed:100,
+    backDelay: 100,
+    loop: true,
+
+});
+
+ScrollReveal({
+    reset: true,
+    distance:'80px',
+    duration:200,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content,heading',{origin:'top'});
+ScrollReveal().reveal('.home-img img,services-container, .portfolio-box, .testimonial-wrapper, .contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1 ,.about-img img',{origin:'left'});
